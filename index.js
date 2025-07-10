4d1b924ad0fb1c17af4b031fb1d8c0a15af372c1
DelTa-0 <aryalmadhave123@gmail.com>
2025-07-10 22:20:03 +0545

    setup more file structure

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import userRouter from './routes/userRouter.js';
+import cookieParser from 'cookie-parser';
+import ejs from 'ejs';
 const app=express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
-
+app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
     res.send("hello working")
